674ce32ecd4861208ad877e0d2ef752fa942b804
Lee-bitwise <lielttato27@gmail.com>
2021-07-23 15:37:56 -0500

    addmorequote

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ let quotes = [
        ' "Genius is one percent inspiration and ninety-nine percent perspiration. -Thomas Edison"',
        ' "You can observe a lot just by watching.-Yogi Berra"',
         '"A house divided against itself cannot stand. -Abraham Lincoln"',
+        '"Difficulties increase the nearer we get to the goal. -Johann Wolfgang von Goethe"',
+       ' "Fate is in your hands and no one elses. -Byron Pulsifer"',
+       '"Be the chief but never the lord. -Lao Tzu"',
 ];
 
 buttonid.addEventListener('click',function(){
